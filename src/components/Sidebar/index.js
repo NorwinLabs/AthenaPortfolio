034017453f1e3ef48ec3df8ac10f9a60f43eeb6a
Gavin Norwood <pokelover222222@gmail.com>
2023-10-24 09:52:28 -0400

    Improved Portfolio app with react

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,26 @@
+import './index.scss'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+
+
+const Sidebar = () => (
+    <div className='nav-bar'> 
+        <Link className='logo' to='/'>
+            <img src={LogoS} alt="logo"/>
+            <img className="sub-logo" src={LogoSubtitle} alt="slobodan"/>
+        </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon  icon={faHome} color='#4d4d4e'/>
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                <FontAwesomeIcon  icon={faUser} color='#4d4d4e'/>
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="comtact-link" to="/contact">
+                <FontAwesomeIcon  icon={faEnvelope} color='#4d4d4e'/>
+            </NavLink>
+        </nav>
+    </div>
+)
+
+export default Sidebar
