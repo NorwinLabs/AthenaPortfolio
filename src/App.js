d9291e0e0ad0f7e1671067cb78406a12e6abe794
Gavin Norwood <pokelover222222@gmail.com>
2023-10-25 14:39:26 -0400

    Finished up siet chages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import logo from './logo.svg';
-import './App.scss';
-import { Routes, Route } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
 import Layout from './components/Layout'
+import Portfolio from './components/Portfolio'
+import Dashboard from './components/Dashboard'
+import './App.scss'
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Layout />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+      </Routes>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
